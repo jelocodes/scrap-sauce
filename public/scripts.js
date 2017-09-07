b7bc679f37af728f8fb5e3f55ff91b810dcdd86c
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2017-09-07 17:08:49 -0400

    edit styles and add logic functions

--- public/scripts.js
+++ public/scripts.js
@@ -21,3 +21,152 @@ scrapSauce.ingredientsSpan = $('#ingredients');
 scrapSauce.manualAddField = $('#addField');
 scrapSauce.manualAddButton = $('.manualAdd a');
 
+// Functions
+
+scrapSauce.removeFromIngredients = function(element){
+    var index = scrapSauce.ingredients.indexOf(element.text().toLowerCase());
+    scrapSauce.ingredients.splice(index, 1);
+};
+
+scrapSauce.addRecipe = function(id){
+
+    // add recipe id to scrapSauce.recipes array
+    scrapSauce.recipes.push(id);
+
+    // make AJAX call to get full recipe
+
+    $.ajax({
+        url: 'https://api.yummly.com/v1/api/recipe/' + id,
+        type: 'GET',
+        data: {
+            format: 'jsonp',
+            _app_key: scrapSauce.apikey,
+            _app_id: scrapSauce.appId
+        },
+        dataType: 'jsonp',
+        success: function(recipe){
+             console.log(recipe);
+             // Update ".chosenMeals" to display the fetched recipe
+             scrapSauce.addFavourite(recipe);
+        }
+    });
+};
+
+scrapSauce.manuallyAdd = function(inputField){
+
+    inputField.keypress(function(event){
+
+        if (event.which === 13){
+
+            var newContent = inputField.val();
+            inputField.val("");
+
+            // I'm adding this weird if statement because the program was buggy and kept looping twice, three times, four times on manual add. Don't know why, should figure out.
+
+            if (newContent) {
+
+                scrapSauce.ingredients.push(newContent);
+
+                if (inputField.attr("id") == "editField") {
+
+                    inputField.parent().html(newContent + "<a href='#' class='delete' onclick='event.preventDefault();'><i class='fa fa-times'></i></a>");
+
+                    // super sketch about having to put this function here:
+
+                    $('.editable .delete').on('click', function(){
+
+                        // if the ingredient was added from the NoFrills flyer, unhighlight it
+                        var editableSpan = $(this).parent();
+                        var highlightedSpan = $('#' + editableSpan.text().replace(/\s/, ''));
+                        if (highlightedSpan.length) {
+                            highlightedSpan.removeClass("selected");
+                        }
+
+                        // remove the ingredient from mealPlan.ingredients
+                        scrapSauce.removeFromIngredients(editableSpan);
+
+                        scrapSauce.updateh2();
+
+                        if (!$.isEmptyObject(scrapSauce.ingredients)) {
+                            scrapSauce.getRecipes(scrapSauce.ingredients);
+                        }
+
+                    });
+
+                } else { // the input field is "addField" from manual input.
+
+                    scrapSauce.updateh2();
+
+                    inputField.slideUp();
+                }
+                
+                // fire a new AJAX call for the new ingredients
+                scrapSauce.getRecipes(scrapSauce.ingredients);
+            }
+        }
+    });
+};
+
+scrapSauce.updateh2 = function(){
+
+    // if mealPlan.ingredients is empty, make h2 display "..." again; otherwise, update h2 with the new list of ingredients.
+    if ($.isEmptyObject(scrapSauce.ingredients)) {
+        scrapSauce.ingredientsSpan.html("...");
+        scrapSauce.generatedRecipesList.html("");
+    } else {
+        scrapSauce.ingredientsSpan.html(
+            '<span class="editable">' + scrapSauce.ingredients.join('<a href="#" class="delete" onclick="event.preventDefault();"><i class="fa fa-times"></i></a></span> and <span class="editable">') + '<a href="#" class="delete" onclick="event.preventDefault();"><i class="fa fa-times"></i></a></span>'
+        );
+        $('p.hint').show();
+    }
+
+    $('.editable').dblclick(function(){
+
+        var innerText = $(this).text();
+
+        // temporarily remove the double-clicked ingredient from mealPlan.ingredients
+        scrapSauce.removeFromIngredients($(this));
+
+        // Turn inner text html into an input field CONTAINING the inner text
+        $(this).html(
+            '<input type="text" id="editField" value="' + innerText + '">'
+        );
+
+        scrapSauce.manuallyAdd($('#editField'));
+
+    });
+
+    $('.editable .delete').on('click', function(){
+
+        // remove the ingredient from mealPlan.ingredients
+        scrapSauce.removeFromIngredients(editableSpan);
+
+        scrapSauce.updateh2();
+
+        if (!$.isEmptyObject(scrapSauce.ingredients)) {
+            scrapSauce.getRecipes(scrapSauce.ingredients);
+        }
+
+    });
+
+};
+
+scrapSauce.getRecipes = function(ingredient){
+    $.ajax({
+        url: 'https://api.yummly.com/v1/api/recipes',
+        type: 'GET',
+        data: {
+            format: 'jsonp',
+            _app_key: scrapSauce.apikey,
+            _app_id: scrapSauce.appId,
+            allowedIngredient: ingredient
+        },
+        dataType: 'jsonp',
+        success: function(result){
+             console.log("here I'm logging just 'result':");
+             console.log(result);
+             console.log(result.matches);
+             scrapSauce.displayRecipes(result.matches);
+        }
+    });
+};

9b6e95bbcb34b07a18cd3630c93864ef952843d0
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2017-09-07 16:01:43 -0400

    import styling and begin scripts

---
+++ public/scripts.js
@@ -0,0 +1,15 @@
+// Entire App
+
+var scrapSauce= {};
+
+// selected ingredients
+scrapSauce.ingredients = [];
+
+// favourited recipes
+scrapSauce.recipes = [];
+
+// Yummly API info
+
+scrapSauce.apikey = "f235b2cf2658a138b83a42670e7c1374";
+scrapSauce.appId = "441e00da";
+
